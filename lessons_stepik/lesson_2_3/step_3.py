f3eab768554bf6431d43a53446693dbb340fad4f
trystep <alexeevmakc@mail.ru>
2021-03-11 18:10:28 +0300

    commit

---
+++ lessons_stepik/lesson_2_3/step_3.py
@@ -0,0 +1,9 @@
+# Открыть страницу http://suninjuly.github.io/alert_accept.html
+# Нажать на кнопку
+# Принять confirm
+# На новой странице решить капчу для роботов, чтобы получить число с ответом
+
+from selenium import webdriver
+from time import sleep
+
+
